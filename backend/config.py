4c6b4b67832cc9d8e5e7b5ce3d83d59120b5d1c6
AurelioAlfons <yuroalfons0407@gmail.com>
2025-05-08 14:58:21 +1000

    Enable cloud db connection with Railway

--- backend/config.py
+++ backend/config.py
@@ -3,12 +3,22 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Loads environment variables from your .env file
 
+# This is localhost MySQL
+# DB_CONFIG = {
+#     'host': os.getenv('DB_HOST'),
+#     'user': os.getenv('DB_USER'),
+#     'password': os.getenv('DB_PASSWORD'),
+#     'database': os.getenv('DB_NAME'),
+#     'port': int(os.getenv('DB_PORT'))
+# }
+
+# This allows for the cloud Railway connection
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST'),
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
-    'database': os.getenv('DB_NAME'),
-    'port': int(os.getenv('DB_PORT'))
+    "host": "maglev.proxy.rlwy.net",
+    "port": 32730,
+    "user": "root",
+    "password": "rTKmCwVmNWWCRYysNsaRMuLWXDHsqMbw",
+    "database": "railway"
 }
 
 # DB_CONFIG = {
