3192dd834a45ab5833b9a1d6469822f7e91164fb
AurelioAlfons <yuroalfons0407@gmail.com>
2025-05-26 22:11:42 +1000

    Clean

--- routes/statistics_routes.py
+++ routes/statistics_routes.py
@@ -9,7 +9,6 @@
 
 from flask import Blueprint, request, jsonify
 from backend.analytics.statistics import get_summary_stats
-from backend.analytics.season_stats.season_stats import get_seasonal_stats
 
 stats_bp = Blueprint('stats_bp', __name__)
 
@@ -21,10 +20,10 @@ def api_summary_stats():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-@stats_bp.route('/api/seasonal_stats', methods=['POST'])
-def api_seasonal_stats():
-    try:
-        data = request.get_json()
-        return jsonify(get_seasonal_stats(int(data['year']), data['time'], data['traffic_type'])), 200
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+# @stats_bp.route('/api/seasonal_stats', methods=['POST'])
+# def api_seasonal_stats():
+#     try:
+#         data = request.get_json()
+#         return jsonify(get_seasonal_stats(int(data['year']), data['time'], data['traffic_type'])), 200
+#     except Exception as e:
+#         return jsonify({"status": "error", "message": str(e)}), 500
